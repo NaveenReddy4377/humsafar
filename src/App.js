2fc6d559b1e348ed78e215a6ae6dba56d6edb331
NaveenReddy4377 <gadipallinaveenreddy@gmail.com>
2023-07-26 21:10:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Card from './component/card';
+import DataCard from './component/card';
+import Header from './component/header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './component/about';
+import TotalPrediction from './component/total-prediction';
+import FormForInsertion from './component/formforinsertion';
+import Privacy from './component/privacy';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Header/>
+    <div className='header'>
+      <div>
+      <div className='heading'>Humsafar</div>
+      </div>
+      </div>
+    <Routes>
+    <Route path='/' element={<DataCard/>}></Route>
+    <Route path="/about" element={<About />}></Route>
+    <Route path="/totalprediction" element={<TotalPrediction />}></Route>
+    <Route path="/05499b5ef634" element={<FormForInsertion />}></Route>
+    <Route path="/Privacypolicy" element={<Privacy/>}></Route>
+      </Routes>
+     
+    </BrowserRouter>
+    </>
   );
 }
 
